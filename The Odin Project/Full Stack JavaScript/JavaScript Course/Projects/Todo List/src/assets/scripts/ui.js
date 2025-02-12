3f7de1e7a1bbd67cf986d89d5e41e8a0a59f1199
Dev_Andy <andrealeman.contact@gmail.com>
2025-02-12 11:21:16 -0600

    Finished main styling:
    - Aside's width is handled properly now.
    * Todo:
    - Main functionality of the site at 'index.js'.

--- The Odin Project/Full Stack JavaScript/JavaScript Course/Projects/Todo List/src/assets/scripts/ui.js
+++ The Odin Project/Full Stack JavaScript/JavaScript Course/Projects/Todo List/src/assets/scripts/ui.js
@@ -19,22 +19,46 @@ const aside = document.querySelector('aside');
 const div = aside.querySelector('div');
 const bars = div.querySelector('.fa-bars');
 
+function updateBarsPosition() {
+  bars.style.left = aside.getBoundingClientRect().width + 5 + 'px';
+}
+
+updateBarsPosition();
+
 bars.addEventListener('click', () => {
-  if (aside.style.width === '0px') {
-    bars.style.left = '305px';
-    aside.style.width = '300px';
+  if (aside.style.width === '0px' || !aside.style.width) {
+    aside.style.display = 'block'; 
+    aside.style.width = '20vw';
+    aside.style.maxWidth = '325px';
+    aside.style.minWidth = '200px';
+    aside.style.height = '100vh';
+    aside.style.visibility = 'visible';
+
     aside.querySelectorAll('*').forEach((el) => {
-      if (el !== div && el !== bars) el.style.display = '';
-      el.removeAttribute('aria-hidden');
+      if (el !== div && el !== bars) {
+        el.style.display = ''; 
+        el.removeAttribute('aria-hidden');
+      }
     });
+
+    setTimeout(updateBarsPosition, 150);
   } else {
     bars.style.left = '5px';
-    aside.style.width = '0px';
+
+    aside.style.width = '0';
+    aside.style.minWidth = '0';
+
     aside.querySelectorAll('*').forEach((el) => {
       if (el !== div && el !== bars) {
         el.style.display = 'none';
         el.setAttribute('aria-hidden', 'true');
       }
     });
+
+    setTimeout(updateBarsPosition, 150);
   }
+});
+
+window.addEventListener('resize', () => {
+  setTimeout(updateBarsPosition, 125);
 });
