dd0db4e629d1abb8372bed101b49e0ce649d5214
Andre <andre.aleman.contact@gmail.com>
2025-03-21 11:12:04 -0600

    Testing Practice project

---
+++ Full Stack JavaScript/JavaScript Course/Projects/Testing Practice/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader'], 
+      },
+    //   {
+    //     test: /\.css$/,
+    //     use: ['style-loader', 'css-loader'], 
+    //   },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  mode: 'development',
+};
