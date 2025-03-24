bb083bdad1cf87373377a8fa875da49ebbd75bbb
Andre <andre.aleman.contact@gmail.com>
2025-03-23 23:51:13 -0600

    Your descriptive commit message here

--- Full Stack JavaScript/JavaScript Course/Projects/Testing Practice/webpack.config.js
+++ Full Stack JavaScript/JavaScript Course/Projects/Testing Practice/webpack.config.js
@@ -1,32 +1,39 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.html$/,
-        use: ['html-loader'], 
+        use: ['html-loader'],
       },
-    //   {
-    //     test: /\.css$/,
-    //     use: ['style-loader', 'css-loader'], 
-    //   },
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './src/template.html',
     }),
   ],
-  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000, 
+    open: true, 
+    hot: true, 
+    watchFiles: ['src/**/*'], 
+  },
+  mode: 'development', 
 };
